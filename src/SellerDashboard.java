845104d948f226ac11e62dbaa2e6eb8166077684
arayapatel <pate1760@purdue.edu>
2022-11-06 18:14:21 -0500

    update

--- src/SellerDashboard.java
+++ src/SellerDashboard.java
@@ -1,3 +1,25 @@
-public class SellerDashboard extends Dashboard{
+package src;
+
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+public class SellerDashboard {
+    public ArrayList<String> readFile() {
+        ArrayList<String> buyers = new ArrayList<String>();
+        try {
+            BufferedReader bfr = new BufferedReader(new FileReader(new File("Buyer.txt")));
+            String line = bfr.readLine();
+            while (line != null) {
+                buyers.add(line);
+                line = bfr.readLine();
+            }
+            bfr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return buyers;
+    }
+
 
 }
