b45538f480e4fdd69c0bc8bc9e9705fde92790d1
Alex <asafonov@purdue.edu>
2022-11-06 15:31:00 -0500

    SampleSeller.txt

--- src/ConversationReaderWriter.java
+++ src/ConversationReaderWriter.java
@@ -1,2 +1,18 @@
 public class ConversationReaderWriter {
+    private String myUser;
+    public String recievingUser;
+    private String conversations;
+
+    public ConversationReaderWriter(String myUser, String recievingUser) {
+        this.myUser = myUser;
+
+    }
+
+    public String[] getAllMessages() {
+        try {
+
+        } catch (Exception e) {
+
+        }
+    }
 }
