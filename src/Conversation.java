c958f8ba3b87f672ad97c2ad21d1163cc53cb3b3
3tas <sdhruvemail@gmail.com>
2022-11-07 21:48:54 -0500

    conversation class

--- src/Conversation.java
+++ src/Conversation.java
@@ -10,6 +10,40 @@ public class Conversation {
     private String recipient;
     private int option = 0;
 
+    public Conversation(Scanner scan, String userName, String fileName, String recipient, String store) {
+        this.scan = scan;
+        this.userName = userName;
+        this.fileName = fileName;
+        this.recipient = recipient;
+        ConversationReaderWriter crw = new ConversationReaderWriter(userName, recipient, store);
+        String[] messageLog = crw.getMessages(); //find exact name
+        ArrayList<ArrayList<String[]>> messages = new ArrayList<ArrayList<String[]>>();
+        String sender = messageLog[0].split(";", 4)[0];
+        String timeString = messageLog[0].split(";", 4)[2];
+        int time = Integer.parseInt(timeString.substring(9, 11)) * 60 + Integer.parseInt(timeString.substring(12, 14));
+        System.out.printf("%s @%d%n", sender, time);
+        for (String message : messageLog) {
+            String[] messageSep = message.split(";", 4);
+            String tempTimeString = messageLog[0].split(";", 4)[2];
+            int tempTime = Integer.parseInt(tempTimeString.substring(9, 11)) * 60
+                    + Integer.parseInt(tempTimeString.substring(12, 14));
+            if (!(messageSep[0].equals(sender) && (Math.abs(tempTime - time) < 30))) {
+                sender = messageSep[0];
+                timeString = messageSep[2];
+                time = Integer.parseInt(timeString.substring(9, 11)) * 60 + Integer.parseInt(timeString.substring(12, 14));
+                System.out.printf("%s @%d%n", sender, time);
+            }
+            System.out.println(messageSep[3]);
+
+        }
+        //System.out.printf("%s"); //usern, read, time, message
+        // xx/xx/xx xx:xx
+        //}
+        do {
+            System.out.println("What do you want to do?/n");
+        } while (option != 5);
+    }
+
     public String getUserName() {
         return userName;
     }
@@ -22,7 +56,6 @@ public class Conversation {
         return fileName;
     }
 
-
     public String getRecipient() {
         return recipient;
     }
@@ -30,39 +63,12 @@ public class Conversation {
     public int getOption() {
         return option;
     }
-
-    public Conversation(Scanner scan, String userName, String fileName, String recipient, String store) {
-        this.scan = scan;
-        this.userName = userName;
-        this.fileName = fileName;
-        this.recipient = recipient;
-        ConversationReaderWriter crw = new ConversationReaderWriter(userName, recipient,store);
-        String[] messageLog = crw.getMessages(); //find exact name
-        ArrayList<ArrayList<String[]>> messages = new ArrayList<ArrayList<String[]>>();
-        String sender = messageLog[0].split(";",4)[0];
-        String time = messageLog[0].split(";",4)[2];
-        System.out.printf("%s @%s%n", sender, time);
-        for (String message : messageLog) {
-            String[] messageSep = message.split(";",4);
-            if (messageSep[0].equals(sender) && /* something to check time*/) {
-                System.out.println(messageSep[3]);
-
-            }
-
-        }
-            //System.out.printf("%s"); //usern, read, time, message
-        //}
-        do {
-            System.out.println("What do you want to do?/n");
-        } while (option != 5);
-    }
-
-    public void sendMessage() {
-        scan.
-
-
-    }
-
+//    public void sendMessage() {
+//        scan.
+//
+//
+//    }
+//
 
 
 }
