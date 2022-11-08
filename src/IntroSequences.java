de1acf7e53741c23c41ea06b6d3a9e4621568eac
nitin-nallagatla <nitinben9@gmail.com>
2022-11-07 23:19:34 -0500

    Updating and Buyer, Seller, IntroSequences

--- src/IntroSequences.java
+++ src/IntroSequences.java
@@ -18,9 +18,10 @@ public class IntroSequences {
         Seller se = new Seller("src/SampleDatabase.txt");
         System.out.println("To login, enter 1. \nTo create an account, enter 2. ");
         Scanner s = new Scanner(System.in);
+        s.nextLine();
         if (s.nextInt() == 1) {
             s.nextLine();
-            System.out.println("If you are a buyer, enter 1. If you are a seller, enter 2.");
+            System.out.println("If you are a buyer, enter 1. \nIf you are a seller, enter 2.");
             int buyer = s.nextInt();
             s.nextLine();
             System.out.println("Please enter username: ");
@@ -58,10 +59,82 @@ public class IntroSequences {
                 System.out.println("failure");
 
         }
+        else if (s.nextInt() == 2) {
+            s.nextLine();
+            System.out.println("If you want to make a buyer account, enter 1. \nIf you are want to make a " +
+                    "seller account, enter 2.");
+            int buyer = s.nextInt();
+            s.nextLine();
+
+            String finalUser;
+            String finalPass;
+
+            if (buyer == 1) {
+                System.out.println("Please enter your new username: ");
+                String usernameNew = s.nextLine();
+                while (!((boolean) verify(usernameNew, false, false)[1])) {
+                    System.out.println("That username is taken! ");
+                }
+                finalUser = (String) verify(usernameNew, false, false)[0];
+
+                System.out.println("Please enter your new password: ");
+                String passwordNew = s.nextLine();
+                while (!((boolean) verify(passwordNew, false, true)[1])) {
+                    System.out.println("That password is taken! ");
+                }
+                finalPass = (String) verify(passwordNew, false, true)[0];
+            }
+            else if (buyer == 2) {
+                System.out.println("Please enter your new username: ");
+                String usernameNew = s.nextLine();
+                while (!((boolean) verify(usernameNew, true, false)[1])) {
+                    System.out.println("That username is taken! ");
+                }
+                finalUser = (String) verify(usernameNew, true, false)[0];
+
+                System.out.println("Please enter your new password: ");
+                String passwordNew = s.nextLine();
+                while (!((boolean) verify(passwordNew, true, true)[1])) {
+                    System.out.println("That password is taken! ");
+                }
+                finalPass = (String) verify(passwordNew, true, true)[0];
+            }
+        }
     }
 
     public static void main(String[] args) {
         IntroSequences is = new IntroSequences();
+    }
+
+    public Object[] verify(String test, boolean seller, boolean password) {
+        Buyer b = new Buyer("src/SampleDatabase.txt");
+        Seller se = new Seller("src/SampleDatabase.txt");
+        boolean success = false;
+
+        if (!seller) {
+            ArrayList<String> takenLines = b.findBuyers();
+            for (String i : takenLines) {
+                if (!password && i.split(",")[0].equals(test)) {
+                    return new Object[]{test, false};
+                } else if (password && i.split(",")[1].equals(test)) {
+                    return new Object[]{test, false};
+                }
+            }
+            return new Object[]{test, true};
+        }
+        else if (seller) {
+            ArrayList<String> takenLines = b.findSellers();
+            for (String i : takenLines) {
+                if (!password && i.split(",")[0].equals(test)) {
+                    return new Object[]{test, false};
+                } else if (password && i.split(",")[1].equals(test)) {
+                    return new Object[]{test, false};
+                }
+            }
+            return new Object[]{test, true};
+        }
+
+        return new Object[0];
     }
 
 //  public static void test() {
