b5f225233ad293cf82c85c367656719ef7e57698
nitin-nallagatla <nitinben9@gmail.com>
2022-11-10 21:37:38 -0500

    Finishing IntroSequences.java

--- src/IntroSequences.java
+++ src/IntroSequences.java
@@ -121,6 +121,21 @@ public class IntroSequences {
         return new Object[]{test, true};
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
 
 //  public static void test() {
